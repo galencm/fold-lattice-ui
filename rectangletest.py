54466d49d074adf992fab7b7b5651522f6c1a32c
Galen Curwen-McAdams <galencm@fastmail.com>
2018-02-04 13:51:08 +0000

    Problem: filled list length can be less than steps
    Solution: check and pad list if needed

--- rectangletest.py
+++ rectangletest.py
@@ -11,13 +11,18 @@ def sequence_status(steps, filled, filename, width=60, height=120):
     status_tile = PILImage.new('RGB', (width, height), (155, 155, 155, 1))
     draw = ImageDraw.Draw(status_tile)
 
-    for step_num, step in enumerate(filled[:steps]):
+    if len(filled) >= steps:
+        sequence_steps = filled[:steps]
+    else:
+        sequence_steps = filled + [None for _ in range(steps - len(filled))]
+
+    for step_num, step in enumerate(sequence_steps):
         if step is None:
             color = "gray"
         else:
             color = "lightgray"
         stepwise = height / steps
-        draw.rectangle((0, stepwise * step_num, width, (stepwise * step_num) + stepwise), outline=None, fill=color)
+        draw.rectangle((0, stepwise * step_num, width, (stepwise * step_num) + stepwise), outline=(223,223,223,1), fill=color)
 
     image_filename = '/tmp/{}.jpg'.format(filename)
     status_tile.save(image_filename)
