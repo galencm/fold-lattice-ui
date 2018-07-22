813bf2e3033565f8d5365361f199de60ca3931e1
Galen Curwen-McAdams <galencm@fastmail.com>
2018-07-22 07:11:47 +0000

    Problem: AccordionContainer cannot close all folds
    Solution: change Accordion do_layout method to allow
    all children to be collapsed. Comment out Logger in
    display_space check.

--- fold_ui/fold_lattice_ui.py
+++ fold_ui/fold_lattice_ui.py
@@ -1336,6 +1336,57 @@ class AccordionContainer(Accordion):
         self.open_column_position = 0
         super(AccordionContainer, self).__init__(anim_duration=0, min_space=self.folded_fold_width)
 
+    # copy paste _do_layout to override 'if all_collapsed:'
+    def _do_layout(self, dt):
+        children = self.children
+        if children:
+            all_collapsed = all(x.collapse for x in children)
+        else:
+            all_collapsed = False
+
+        if all_collapsed:
+            # allow all to be collapsed
+            children[0].collapse = True
+
+        orientation = self.orientation
+        min_space = self.min_space
+        min_space_total = len(children) * self.min_space
+        w, h = self.size
+        x, y = self.pos
+        if orientation == 'horizontal':
+            display_space = self.width - min_space_total
+        else:
+            display_space = self.height - min_space_total
+
+        if display_space <= 0:
+            # Logger.warning('Accordion: not enough space '
+            #                'for displaying all children')
+            # Logger.warning('Accordion: need %dpx, got %dpx' % (
+            #     min_space_total, min_space_total + display_space))
+            # Logger.warning('Accordion: layout aborted.')
+            return
+
+        if orientation == 'horizontal':
+            children = reversed(children)
+
+        for child in children:
+            child_space = min_space
+            child_space += display_space * (1 - child.collapse_alpha)
+            child._min_space = min_space
+            child.x = x
+            child.y = y
+            child.orientation = self.orientation
+            if orientation == 'horizontal':
+                child.content_size = display_space, h
+                child.width = child_space
+                child.height = h
+                x += child_space
+            else:
+                child.content_size = w, display_space
+                child.width = w
+                child.height = child_space
+                y += child_space
+
     def view_next(self):
         try:
             self.app.session["sources"].view_selector.viewer_next()
